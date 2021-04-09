37fc5ad5b14e4c9ac9da2dc4cf034a302b3a84e6
smirok <smirok123@mail.ru>
2021-04-10 00:47:09 +0300

    Identify tokens

---
+++ interpreter/src/main/java/Token.java
@@ -0,0 +1,48 @@
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum Token {
+
+    NUMBER("\\d+"),
+    IDENTIFIER("[0-9a-zA-Z_]+"),
+    PLUS("\\+"),
+    MINUS("-"),
+    MULT("\\*"),
+    DIVIDE("/"),
+    MODULE("%"),
+    LT("<"),
+    GT(">"),
+    EQ("="),
+    LEFT_ROUND_BRACKET("\\("),
+    RIGHT_ROUND_BRACKET("\\)"),
+    COMMA(","),
+    LEFT_SQUARE_BRACKET("\\["),
+    RIGHT_SQUARE_BRACKET("\\]"),
+    QUESTION("\\?"),
+    COLON(":"),
+    LEFT_BRACE("\\{"),
+    RIGHT_BRACE("\\}"),
+    EOL("\\n");
+
+    private final String str;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    private String value;
+
+    Token(String str) {
+        this.str = str;
+    }
+
+    public Optional<Integer> indexAfterMatch(String s, int pos) {
+        Matcher matcher = Pattern.compile("^" + str).matcher(s);
+        return matcher.region(pos, s.length()).find() ? Optional.of(matcher.end()) : Optional.empty();
+    }
+}
