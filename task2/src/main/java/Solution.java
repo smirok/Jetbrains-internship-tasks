e3c22b22267c840c939affaf3f78b95ad6151d19
smirok <smirok123@mail.ru>
2021-04-11 16:14:52 +0300

    Replaced future with threads

--- task2/src/main/java/Solution.java
+++ task2/src/main/java/Solution.java
@@ -1,42 +1,41 @@
-import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
 
 public class Solution {
+    private static final int RUNNING_TIME = 5000;
 
     public static boolean matches(String text, String regex) {
-        ExecutorService executor = Executors.newCachedThreadPool();
-        Callable<Boolean> task = new Callable<Boolean>() {
-            public Boolean call() {
-                return Pattern.compile(regex).matcher(text).matches();
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        Thread funcThread = new Thread() {
+            public void run() {
+                try {
+                    atomicBoolean.set(Pattern.compile(regex).matcher(text).matches());
+                } catch (StackOverflowError error) {
+                    atomicBoolean.set(false);
+                }
             }
         };
-        Future<Boolean> future = executor.submit(task);
-        try {
-            Boolean res = future.get(5, TimeUnit.SECONDS);
-            future.cancel(true);
-            executor.shutdownNow();
-            return res;
-        } catch (TimeoutException ex) {
-            future.cancel(true);
-            executor.shutdownNow();
-            return false;
-        } catch (InterruptedException | ExecutionException e) {
-            executor.shutdownNow();
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(RUNNING_TIME);
+                } catch (InterruptedException ignored) {
+                }
+            }
+        };
+
+        timerThread.setDaemon(true);
+        timerThread.start();
+
+        funcThread.setDaemon(true);
+        funcThread.start();
+
+        while (true) {
+            if (!timerThread.isAlive() || !funcThread.isAlive()) {
+                return atomicBoolean.get();
+            }
         }
-        return false;
-    }
-
-    public static void main(String[] args) {
-        String p = "(a|aa)+";
-
-        StringBuilder s = new StringBuilder();
-        for (; s.length() < 10_000_000; s.append('a')) {
-        }
-
-        System.out.println(matches(s.toString(), p));
-
-        System.out.println(matches("aaaaa", p));
-
-        return;
     }
 }
