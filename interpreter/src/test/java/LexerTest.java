5b49614d62576bde244617baf864f5c2d9683cf9
smirok <smirok123@mail.ru>
2021-04-11 13:59:11 +0300

    add error test and fix return value

--- interpreter/src/test/java/LexerTest.java
+++ interpreter/src/test/java/LexerTest.java
@@ -6,56 +6,61 @@ public class LexerTest {
     @Test
     public void testLexSimple() {
         Lexer lexer = new Lexer("(2+2)");
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.PLUS);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.PLUS);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
     }
 
     @Test
     public void testLexMiddle() {
         Lexer lexer = new Lexer("(2+((3*4)/5))");
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.PLUS);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.MULT);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.DIVIDE);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.PLUS);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.MULT);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.DIVIDE);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
     }
 
     @Test
     public void testLexHard() {
         Lexer lexer = new Lexer("[((10+20)>(20+10))]?{1}:{0}");
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_SQUARE_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.PLUS);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.GT);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.PLUS);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_ROUND_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_SQUARE_BRACKET);
-        Assertions.assertEquals(lexer.findNextToken(), Token.QUESTION);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_BRACE);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_BRACE);
-        Assertions.assertEquals(lexer.findNextToken(), Token.COLON);
-        Assertions.assertEquals(lexer.findNextToken(), Token.LEFT_BRACE);
-        Assertions.assertEquals(lexer.findNextToken(), Token.NUMBER);
-        Assertions.assertEquals(lexer.findNextToken(), Token.RIGHT_BRACE);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_SQUARE_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.PLUS);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.GT);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.PLUS);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_ROUND_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_SQUARE_BRACKET);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.QUESTION);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_BRACE);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_BRACE);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.COLON);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.LEFT_BRACE);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.NUMBER);
+        Assertions.assertEquals(lexer.findNextToken().getKey(), Token.RIGHT_BRACE);
+    }
+
+    @Test
+    public void testLexError() {
+        Assertions.assertFalse(new Lexer("hello world@").lexAll());
     }
 }
